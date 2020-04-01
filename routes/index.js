c1f68197da678be554453ac2b0c52beb2beb8080
Beaule <frederic.lebeau.mobile@gmail.com>
2020-04-01 16:31:13 +0200

    UX

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ var router = express.Router();
  ************************************/
 /* GET home login/register page. */
 router.get('/', function (req, res, next) {
-  res.redirect("/sources");
+  res.redirect("/pods");
 });
 
 
