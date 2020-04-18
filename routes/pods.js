f0b0579941362403ca7914352e080b2dee786ca9
Beaule <frederic.lebeau.mobile@gmail.com>
2020-04-18 21:52:52 +0200

    pod

--- routes/pods.js
+++ routes/pods.js
@@ -8,6 +8,8 @@
 import express from 'express';
 import User from '../server/lib/utils/user';
 var router = express.Router();
+import config from '../config/index';
+var ManagementClient = require('auth0').ManagementClient;
 
 /***********************************
  * Private functions
@@ -19,12 +21,69 @@ var router = express.Router();
 
 /* GET dashboard home */
 router.get('/auth/pods', function (req, res, next) {
-  renderPods(req,res);
+  var management = new ManagementClient({
+    domain: config.auth0Domain,
+    clientId: config.auth0ManagementClientID,
+    clientSecret: config.auth0ManagementClientSecret,
+    scope: 'read:clients read:client_keys'
+    });
+    management.getClient(
+      {
+        client_id: User.getApplicationId(req.user)
+      }, function (err, client) {
+        if(err || client==null)
+          renderPods(req,res);
+        else{
+          var podId=client.client_metadata.podTypeId;
+          renderPodsSelected(req,res,podId);
+        }
+  });
+  
 });
 
 /* GET select pod */
 router.get('/auth/pods/select', function (req, res, next) {
-  renderPodsSelect(req,res);
+  var management = new ManagementClient({
+    domain: config.auth0Domain,
+    clientId: config.auth0ManagementClientID,
+    clientSecret: config.auth0ManagementClientSecret,
+    scope: 'read:clients read:client_keys'
+    });
+    management.getClient(
+      {
+        client_id: User.getApplicationId(req.user)
+      }, function (err, client) {
+        if(err || client==null)
+          renderPodsSelect(req,res);
+        else{
+          var podId=client.client_metadata.podTypeId;
+          renderPodsSelected(req,res,podId);
+        }
+  });
+  
+});
+
+/* GET select pod */
+router.get('/auth/pods/form', function (req, res, next) {
+  var podId=req.query.podId;
+
+  var management = new ManagementClient({
+    domain: config.auth0Domain,
+    clientId: config.auth0ManagementClientID,
+    clientSecret: config.auth0ManagementClientSecret,
+    scope: 'update:clients'
+    });
+    management.updateClient(
+      { client_id: User.getApplicationId(req.user)},
+      {"client_metadata":{"podTypeId":podId}}, function (err, client) {
+        console.log(err);
+        if(err)
+          renderPodsSelect(req,res);
+        else{
+          renderPodsSelected(req,res,podId);
+        }
+          
+  });
 });
 
 /***********************************
@@ -53,6 +112,24 @@ function renderPodsSelect(req,res){
   res.render('pods-select', {
     layout: 'master',
     pods:'active',
+    user:{id:User.getUserId(req.user)}
+  });
+}
+
+
+/**
+ * render pod selected 
+ * @param {req} request
+ * @param {res} response
+ */
+function renderPodsSelected(req,res,podId){
+  var isGoogleDrive=false;
+  if(podId=="9ef6b81a414b2432ec6e3d384c5a36cea8aa0c30d3dd2b67364126ed80856f9c20654f032eef87ad981187da8c23c1186eefe1503714835c2e952bbb3f22729c")
+    isGoogleDrive=true;
+  res.render('pods-selected', {
+    layout: 'master',
+    pods:'active',
+    pod:{podId:podId,isGoogleDrive:isGoogleDrive},
     user:{id:User.getUserId(req.user)}
   });
 }
