9b6049f407131400afba95995263b6237b44ba0b
Beaule <frederic.lebeau.mobile@gmail.com>
2020-04-19 16:24:12 +0200

    try access token directly + give client grant to created application

--- routes/applications.js
+++ routes/applications.js
@@ -11,18 +11,13 @@ var router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 import sanitizeBody from 'express-validator/filter';
 import config from '../config/index';
+import request from 'request';
 var ManagementClient = require('auth0').ManagementClient;
 
 /***********************************
  * Private functions
  ************************************/
-
-/***********************************
- * routes functions
- ************************************/
-
-/* GET dashboard home */
-router.get('/auth/applications', function (req, res, next) {
+function getClient(req,cb){
   var management = new ManagementClient({
     domain: config.auth0Domain,
     clientId: config.auth0ManagementClientID,
@@ -33,10 +28,18 @@ router.get('/auth/applications', function (req, res, next) {
       {
         client_id: User.getApplicationId(req.user)
       }, function (err, client) {
-        if(err || client==null)
-          renderApplications(req,res);
-        else
-          renderApplications(req,res,client);
+        cb(client) ;
+  });
+}
+
+/***********************************
+ * routes functions
+ ************************************/
+
+/* GET dashboard home */
+router.get('/auth/applications', function (req, res, next) {
+  getClient(req,function(client){
+    renderApplications(req,res,client);
   });
 });
 
@@ -57,7 +60,7 @@ router.post('/auth/applications/create', function (req, res, next) {
       domain: config.auth0Domain,
       clientId: config.auth0ManagementClientID,
       clientSecret: config.auth0ManagementClientSecret,
-      scope: 'create:clients'
+      scope: 'create:clients create:client_grants'
       });
       management.createClient(
         {
@@ -75,12 +78,53 @@ router.post('/auth/applications/create', function (req, res, next) {
           if(err)
             renderApplicationsForm(req,res,err);
           else{
-            User.setApplicationId(req.user,client.client_id,function (profile){
-              renderApplications(req,res,client);
-            });  
-          }
-            
+            //update grant to enable accesstoken creation by created application
+            var clientId=client.client_id;
+            management.createClientGrant(
+              {
+                client_id: clientId,
+                audience: "https://datavillage.eu.auth0.com/api/v2/",
+                scope: ["read:clients"]
+              }, function (err, grant) {
+                if(err)
+                  renderApplicationsForm(req,res,err);
+                else{
+                  User.setApplicationId(req.user,clientId,function (profile){
+                    renderApplications(req,res,client);
+                  });  
+                }
+                  
+          });
+          }  
     });
+});
+
+/* POST try application access token end point */
+router.post('/auth/applications/test/oauth/token', function (req, res, next) {
+  var clientId=req.body.clientId;
+  var clientSecret=req.body.clientSecret;
+
+  //create pod
+  var options = {
+    'method': 'POST',
+    'url': 'https://api.datavillage.me/oauth/token',
+    'headers': {
+        'Content-Type': ['application/x-www-form-urlencoded', 'application/x-www-form-urlencoded']
+    },
+    form: {
+        'client_id': clientId,
+        'client_secret': clientSecret
+    }
+    };
+    request(options, function (error, response) { 
+      if(response !=null)
+        req.session.applicationAccessToken=JSON.parse(response.body).access_token;
+        console.log(response.body.access_token);
+        getClient(req,function(client){
+          renderApplications(req,res,client);
+        });
+    });
+
 });
 
 /***********************************
@@ -98,7 +142,7 @@ function renderApplications(req,res,client){
       layout: 'master',
       applications:'active',
       user:{id:User.getUserId(req.user)},
-      application:{name:client.name,clientId:client.client_id,clientSecret:client.client_secret,url:client.description,callbacks:client.callbacks}
+      application:{name:client.name,clientId:client.client_id,clientSecret:client.client_secret,url:client.description,callbacks:client.callbacks,applicationAccessToken:req.session.applicationAccessToken}
     });
   }
   else{
