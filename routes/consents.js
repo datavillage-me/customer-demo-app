7392c561277d36ea41c6f04be03cf3ab5dea5e2b
Beaule <frederic.lebeau.mobile@gmail.com>
2020-04-24 23:05:09 +0200

    fix

--- routes/consents.js
+++ routes/consents.js
@@ -8,10 +8,30 @@
 import express from 'express';
 import User from '../server/lib/utils/user';
 var router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+import sanitizeBody from 'express-validator/filter';
+import config from '../config/index';
+import request from 'request';
+var ManagementClient = require('auth0').ManagementClient;
 
 /***********************************
  * Private functions
  ************************************/
+
+function getClient(req,cb){
+  var management = new ManagementClient({
+    domain: config.auth0Domain,
+    clientId: config.auth0ManagementClientID,
+    clientSecret: config.auth0ManagementClientSecret,
+    scope: 'read:clients read:client_keys'
+    });
+    management.getClient(
+      {
+        client_id: User.getApplicationId(req.user)
+      }, function (err, client) {
+        cb(client) ;
+  });
+}
 
 /***********************************
  * routes functions
@@ -20,6 +40,13 @@ var router = express.Router();
 /* GET dashboard home */
 router.get('/auth/consents', function (req, res, next) {
   renderConsents(req,res);
+});
+
+/* GET form */
+router.get('/auth/consents/form', function (req, res, next) {
+  getClient(req,function(client){
+      renderConsentsForm(req,res,client);
+  });
 });
 
 
@@ -36,6 +63,23 @@ function renderConsents(req,res){
   res.render('consents', {
     layout: 'master',
     consents:'active',
+    user:{id:User.getUserId(req.user)}
+  });
+}
+
+/**
+ * render dashboard home
+ * @param {req} request
+ * @param {res} response
+ */
+function renderConsentsForm(req,res,client){
+  var companyUri="";
+  if(client!=null && client.client_metadata!=null)
+    companyUri=client.client_metadata.companyUri;
+  res.render('consents-form', {
+    layout: 'master',
+    consents:'active',
+    application:{companyUri:companyUri},
     user:{id:User.getUserId(req.user)}
   });
 }
